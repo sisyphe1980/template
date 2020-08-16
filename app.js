88417828fc0b055bb8230c235e776c143a93a3c7
sisyphe1980 <sisyphe1980@hotmail.com>
2020-08-16 19:07:26 +0200

    ajout du logger morgan

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const morgan = require('morgan');
 let app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(morgan('combined'));
 
 app.listen(3000);
 console.log("Is run on 3000");
